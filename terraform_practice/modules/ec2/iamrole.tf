1cb18e2554e7e1789bbc94d9c9e2abb0614c9a08
veraleh <pangveraleeh@gmail.com>
2024-12-18 06:37:59 +0100

    creating the iam role

---
+++ terraform_practice/modules/ec2/iamrole.tf
@@ -0,0 +1,13 @@
+resource "aws_iam_role" "ec2_instance" {
+  name               = "my-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
