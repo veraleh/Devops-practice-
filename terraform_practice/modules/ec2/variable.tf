7e842c96e1069e8524ed1c28170e5cac15ad0872
veraleh <pangveraleeh@gmail.com>
2024-12-18 07:30:47 +0100

    The configured iam role

--- terraform_practice/modules/ec2/variable.tf
+++ terraform_practice/modules/ec2/variable.tf
@@ -7,3 +7,8 @@ variable "instance_name" {
 type = string
 description = "this is the ec2 instance name in aws" 
 }
+
+variable "role_name" {
+  type = string
+  description = "This is name of the IAM role"
+}
