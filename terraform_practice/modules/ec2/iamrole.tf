b4b2eed14ab4d660ad12333ebb64002b07c13666
veraleh <pangveraleeh@gmail.com>
2024-12-18 07:30:47 +0100

    The configured iam role

--- terraform_practice/modules/ec2/iamrole.tf
+++ terraform_practice/modules/ec2/iamrole.tf
@@ -1,5 +1,10 @@
+variable "role_name" {
+  type = string
+  description = "This is the name of the IAM role"
+}
+
 resource "aws_iam_role" "ec2_instance" {
-  name               = "my-role"
+  name               = var.role_name
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
